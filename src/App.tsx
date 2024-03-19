c5b3f040a6045d5ee0e1213986e7e98b60e342d2
nnivxix <adhe.hanasa@gmail.com>
2024-03-19 13:23:25 +0700

    wip: show trending

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Response, Movie } from "./types/response";
 
 function App() {
+	const [movies, setMovies] = useState<Movie[]>();
+	const API_TOKEN = import.meta.env.VITE_TMDB_API_TOKEN;
+
+	useEffect(() => {
+		const fetchMovies = async () => {
+			const response = await fetch(
+				"https://api.themoviedb.org/3/trending/all/day",
+				{
+					headers: {
+						Authorization: "Bearer " + API_TOKEN,
+					},
+				}
+			);
+			const data: Response = await response.json();
+			console.log(data);
+			setMovies(data.results);
+		};
+
+		fetchMovies();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<>
 			<Navbar />
+
+			{movies?.map((movie: Movie) => (
+				<h1 key={movie.id}>{movie.title}</h1>
+			))}
 		</>
 	);
 }
