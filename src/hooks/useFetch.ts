ed1b32074b2320d00ff0fced091e585cee822167
nnivxix <adhe.hanasa@gmail.com>
2024-03-20 06:54:51 +0700

    feat: custom hook for fetch data

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,37 +1,35 @@
-/**
- *
- * WIP
- *
- */
-
+import { useState, useEffect } from "react";
 import config from "@/config";
-import { useEffect, useState } from "react";
 
 const API_TOKEN = import.meta.env.VITE_TMDB_API_TOKEN;
 
-async function useFetch<T>(path: string) {
-	const [loading, setLoading] = useState(false);
-	const [data, setData] = useState<T>();
+const useFetch = (path: string) => {
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
-		async function fectData() {
-			const response = await fetch(config.apiUrl + path, {
-				headers: {
-					Authorization: "Bearer " + API_TOKEN,
-				},
-			});
-			const json = await response.json();
-
-			if (json) {
+		const fetchData = async () => {
+			setIsPending(true);
+			try {
+				const response = await fetch(`${config.apiUrl}${path}`, {
+					headers: {
+						Authorization: "Bearer " + API_TOKEN,
+					},
+				});
+				if (!response.ok) throw new Error(response.statusText);
+				const json = await response.json();
+				setIsPending(false);
 				setData(json);
-				setLoading(true);
+				setError(null);
+			} catch (error) {
+				setError(`${error} Could not Fetch Data `);
+				setIsPending(false);
 			}
-		}
-
-		fectData();
+		};
+		fetchData();
 	}, [path]);
-
-	return [data, loading];
-}
+	return { data, isPending, error };
+};
 
 export default useFetch;
--- src/pages/Show/Movie.tsx
+++ src/pages/Show/Movie.tsx
@@ -1,30 +1,28 @@
 import { Movie as MovieType } from "@/types/movie";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import config from "@/config";
+import useFetch from "@/hooks/useFetch";
 
 export default function Movie() {
 	const params = useParams();
 	const [movie, setMovie] = useState<MovieType>();
-	const API_TOKEN = import.meta.env.VITE_TMDB_API_TOKEN;
+	const { data, isPending, error } = useFetch(`/movie/${params.id}`);
 
 	useEffect(() => {
-		async function getDetailMovie() {
-			const response = await fetch(config.apiUrl + "/movie/" + params.id, {
-				headers: {
-					Authorization: "Bearer " + API_TOKEN,
-				},
-			});
-			const data = await response.json();
-			setMovie(data);
-		}
+		setMovie(data!);
+	}, [movie, data, isPending, error]);
 
-		getDetailMovie();
-	}, []);
+	if (error) {
+		return <pre className="text-white">{error}</pre>;
+	}
+	if (isPending) {
+		return <pre className="text-white">loading...</pre>;
+	}
+
 	return (
-		<div>
+		<div className="text-white">
 			<pre>{params.id}</pre>
-			{!!movie && <pre>{movie.original_title}</pre>}
+			<pre>{movie?.original_title}</pre>
 		</div>
 	);
 }
