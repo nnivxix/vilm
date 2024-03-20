1004dfa2047a23ebf95ace3216f71c45c7bf42cb
nnivxix <adhe.hanasa@gmail.com>
2024-03-20 17:04:14 +0700

    fear: show detail movie

--- src/pages/Show/Movie.tsx
+++ src/pages/Show/Movie.tsx
@@ -5,82 +5,119 @@ import type { Response, SimilarMixed, SimilarMovie } from "@/types/response";
 import SimilarCardItem from "@/components/SimilarCardItem";
 import { useState } from "react";
 import { default as dataMovie } from "@/data/movie";
+import movieImages from "@/data/movie-images";
 import { default as similarMoviesData } from "@/data/similar-movies";
 import runtimeDuration from "@/utils/runtime-duration";
 import imageUrl from "@/utils/image-url";
-import movieImages from "@/data/movie-images";
 import pickRandomImages from "@/utils/pick-random-images";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import getYear from "@/utils/get-year";
 
 export default function Movie() {
 	const params = useParams();
 
-	// const { data: similarMovies } = useFetch<Response<SimilarMovie[]>>(
-	// 	`/movie/${params.id}/similar`
-	// );
-	// const {
-	// 	data: movie,
-	// 	isLoading,
-	// 	error,
-	// } = useFetch<MovieType>(`/movie/${params.id}`);
+	const {
+		data: movie,
+		isLoading,
+		error,
+	} = useFetch<MovieType>(`/movie/${params.id}`);
+	const { data: similarMovies } = useFetch<Response<SimilarMovie[]>>(
+		`/movie/${params.id}/similar`
+	);
+	const { data: images } = useFetch<Images>(
+		`/movie/${params.id}/images?include_image_language=null`
+	);
 
-	// const { data: images } = useFetch<Images>(
-	// 	`/movie/${params.id}/images?include_image_language=null`
-	// );
+	// const [images] = useState<Images>(movieImages);
 
-	const [images] = useState<Images>(movieImages);
+	// const [movie, setMovie] = useState<MovieType>(dataMovie);
+	// const [similarMovies] = useState(similarMoviesData);
 
-	const [movie, setMovie] = useState<MovieType>(dataMovie);
-	const [similarMovies] = useState(similarMoviesData);
-
-	// if (error) {
-	// 	return <pre className="text-white">{error}</pre>;
-	// }
-	// if (isLoading) {
-	// 	return <pre className="text-white">loading...</pre>;
-	// }
+	if (error) {
+		return <pre className="text-white">{error}</pre>;
+	}
+	if (isLoading) {
+		return <pre className="text-white">loading...</pre>;
+	}
 
 	return (
 		<div className="text-white">
-			<div className="w-full bg-no-repeat bg-auto relative h-[85vh] ">
-				<div className="grid grid-cols-4 gap-3 mx-60  w-full absolute">
-					<h1 className=" col-span-full text-6xl font-semibold ">
-						{movie.original_title}{" "}
-					</h1>
-					<h4 className="col-span-full">
-						Duration: {runtimeDuration(movie.runtime)}{" "}
-					</h4>
-					<p className="col-span-1 text-lg">{movie.overview}</p>
-					<div className="flex gap-2 col-span-full">
-						{!!movie.genres.length &&
-							movie.genres.map((genre) => (
-								<span className="p-3 border-2 rounded-md hover:bg-white/20 cursor-pointer">
-									{genre.name}
-								</span>
-							))}
+			{!!movie && (
+				<div className="w-full bg-no-repeat bg-auto relative flex justify-center items-center h-screen lg:h-[85vh] ">
+					<div className="grid grid-cols-4 gap-3 mx-auto absolute max-w-6xl px-3">
+						<h1 className=" col-span-full text-6xl font-semibold ">
+							{movie.original_title}{" "}
+						</h1>
+						<h4 className="col-span-full font-semibold">
+							Duration: {runtimeDuration(movie.runtime)} |{" "}
+							{getYear(movie.release_date)}
+						</h4>
+						<p className="col-span-full lg:col-span-2 text-lg">
+							{movie.overview}
+						</p>
+						<div className="flex flex-wrap gap-2 col-span-full">
+							{!!movie.genres.length &&
+								movie.genres.map((genre) => (
+									<span
+										key={genre.id}
+										className="p-3 border-2 rounded-md hover:bg-white/20 cursor-pointer"
+									>
+										{genre.name}
+									</span>
+								))}
+						</div>
+
+						<Carousel className="lg:col-span-full col-span-full ">
+							<CarouselContent>
+								{!!images?.backdrops.length &&
+									pickRandomImages(images.backdrops as Backdrop[]).map(
+										(image: Backdrop) => (
+											<CarouselItem
+												key={image.file_path}
+												className="basis-1/2 lg:basis-1/3"
+											>
+												<img
+													height={200}
+													src={imageUrl({
+														path: image.file_path,
+														size: "w500",
+													})}
+													alt={image.file_path}
+												/>
+											</CarouselItem>
+										)
+									)}
+							</CarouselContent>
+							<CarouselPrevious
+								variant={"ghost"}
+								className="hidden lg:inline-flex "
+							/>
+							<CarouselNext
+								variant={"ghost"}
+								className="hidden lg:inline-flex "
+							/>
+						</Carousel>
 					</div>
 
-					<div className="col-span-full flex ">
-						{!!images?.backdrops.length &&
-							pickRandomImages(images.backdrops as Backdrop[]).map(
-								(image: Backdrop) => (
-									<img
-										src={imageUrl({ path: image.file_path })}
-										alt={image.file_path}
-									/>
-								)
-							)}
-					</div>
+					<div className="bg-black/50 w-full -z-10 h-full absolute"></div>
+					<img
+						src={imageUrl({ path: movie.backdrop_path, size: "w500" })}
+						alt=""
+						className="-z-20 w-full h-full overflow-clip absolute inset-0 bg-fixed bg-center object-cover object-center"
+					/>
 				</div>
-
-				<div className="bg-black/50 w-full -z-10 h-full absolute"></div>
-				<img
-					src={imageUrl({ path: movie.backdrop_path, size: "w500" })}
-					alt=""
-					className="-z-20 w-full h-full overflow-clip absolute inset-0 bg-fixed bg-center object-cover object-center"
-				/>
-			</div>
+			)}
 
 			<div className="grid lg:grid-cols-8 md:grid-cols-4 grid-cols-2 gap-5  mx-auto px-5 mt-5">
+				<h1 className="text-4xl font-semibold col-span-full">
+					Similar Movies:{" "}
+				</h1>
 				{!!similarMovies?.results?.length &&
 					similarMovies?.results?.map((movie) => (
 						<SimilarCardItem
--- src/utils/pick-random-images.ts
+++ src/utils/pick-random-images.ts
@@ -5,13 +5,13 @@ function pickRandomImages(images: Backdrop[]) {
 		throw new Error("Not Array array!");
 	}
 
-	if (images.length < 4) {
+	if (images.length < 7) {
 		return images;
 	}
 
 	const shuffledIndexes = [...images.keys()].sort(() => Math.random() - 0.5);
 	const shuffledImages = shuffledIndexes
-		.slice(0, 4)
+		.slice(0, 7)
 		.map((index) => images[index]);
 
 	return shuffledImages;
