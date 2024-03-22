cbbfc6dfb7518c3e31c6a9086e7fc31ed741511d
kaushalpapnai <kaushalvckm@gmail.com>
2024-03-22 10:44:19 +0530

    added Carito google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily:{
+        carlito: ["Carlito", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
