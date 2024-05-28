778f2db994714dea6d7e4a3430c2709bd03b11ef
Aan abdul rohman <aanabdulrohman3@gmail.com>
2024-05-28 18:47:03 +0700

    feat: fix gird footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,22 @@
 export default function Footer() {
   return (
-    <footer className="bg-slate-950 text-white py-8 px-4 border-t-4 border-blue-600 w-full mt-9">
-      <div className="max-w-md px-4 ">
-        <h1 className="text-2xl font-bold mb-4">Vilm</h1>
-        <p className="text-gray-400 mb-1 ">
-          The Vilm website is a movie discovery platform featuring a modern
-          design. Users can browse various movie titles and access detailed
-          information, including the synopsis and trailers.
-        </p>
-        <div className="flex items-center mb-4">
-          <h3 className="text-gray-400">Data provided by </h3>
-          <img src="tmdb.png" alt="" width={"80px"} className="ml-2" />
+    <footer className="bg-gray-900 text-white py-8 px-4 border-t-4 border-blue-600 w-full mt-9">
+      <div className="w-full px-4 grid grid-cols-2">
+        <div className="col-spans-1">
+          <h1 className="text-2xl font-bold mb-4">Vilm</h1>
+          <p className="text-gray-400 mb-1 ">
+            The Vilm website is a movie discovery platform featuring a modern
+            design. Users can browse various movie titles and access detailed
+            information, including the synopsis and trailers.
+          </p>
+          <div className="flex items-center mb-4">
+            <h3 className="text-gray-400">Data provided by </h3>
+            <a href="https://www.themoviedb.org/">
+              <img src="tmdb.png" alt="" width={"80px"} className="ml-2" />
+            </a>
+          </div>
+          <p className="text-gray-400">© 2024</p>
         </div>
-        <p className="text-gray-400">© 2024</p>
       </div>
     </footer>
   );
