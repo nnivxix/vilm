829d66b12d34767d8d821afaa9c034487ccf3ded
nnivxix <adhe.hanasa@gmail.com>
2025-02-02 17:05:30 +0700

    feat: update robots.txt to block additional Facebook crawler agents

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,8 +3,9 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: "facebookexternalhit",
+      userAgent: ["facebookexternalhit", "meta-externalagent"],
       crawlDelay: 86400,
+      disallow: ["/"],
     },
   };
 }
