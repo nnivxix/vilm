9b43b009c0feeb6eef0e790f4116577b1828d74a
nnivxix <adhe.hanasa@gmail.com>
2025-12-08 15:07:11 +0700

    feat: update domain migration dialog logic and URL variable

--- src/components/MigrateDomain.tsx
+++ src/components/MigrateDomain.tsx
@@ -12,41 +12,38 @@ import {
 import { usePathname } from "next/navigation";
 
 export default function MigrateDomain() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const path = usePathname();
 
   const OLD_DOMAIN = "vilm-react.vercel.app";
-  const newDomain = "https://vilm.hanasa.id" + path + "?utm_source=old_domain";
+  const newUrl = "https://vilm.hanasa.id" + path + "?utm_source=old_domain";
 
   useEffect(() => {
-    console.log("Current hostname:", window.location.hostname);
     if (window.location.hostname === OLD_DOMAIN) {
       setOpen(true);
     } else {
       setOpen(false);
     }
-  }, [setOpen]);
+  }, []);
 
   return (
-    <div>
-      <AlertDialog open={open} onOpenChange={setOpen}>
-        <AlertDialogContent>
-          <AlertDialogTitle>Domain Migration Notice</AlertDialogTitle>
-          <AlertDialogDescription className="space-y-4">
-            We have migrated to{" "}
-            <Link href={newDomain} className="font-semibold hover:underline">
-              https://vilm.hanasa.id
-            </Link>
-            . Please note that you will need to re-enter your authentication
-            token on the new domain.
-          </AlertDialogDescription>
-          <AlertDialogAction>
-            <Link href={newDomain} className="w-full">
-              Go to New Domain
-            </Link>
-          </AlertDialogAction>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogTitle>Domain Migration Notice</AlertDialogTitle>
+        <AlertDialogDescription className="space-y-4">
+          We have migrated to{" "}
+          <Link href={newUrl} className="font-semibold hover:underline">
+            https://vilm.hanasa.id
+          </Link>
+          . Please note that you will need to re-enter your authentication token
+          on the new domain.
+        </AlertDialogDescription>
+        <AlertDialogAction>
+          <Link href={newUrl} className="w-full">
+            Go to New Domain
+          </Link>
+        </AlertDialogAction>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
